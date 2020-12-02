bac19fcec5f97491cf33e8da34216592dc37b38d
Alejandro <ale.antoniobp@gmail.com>
2020-12-01 19:49:04 -0700

    New html pages + js and css changes

--- js/chart.js
+++ js/chart.js
@@ -2,15 +2,15 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
+    
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: 'Cambios de Temperatura',
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            borderColor: '#6FADCF',
+            data: [24, 25, 28, 30, 33, 30, 28]
         }]
     },
 
---
+++ js/chart3.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById('myChart3').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+    
+    // The data for our dataset
+    data: {
+        labels: ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM'],
+        datasets: [{
+            label: 'Cambios de Temperatura',
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            borderColor: '#6FADCF',
+            data: [24, 25, 28, 30, 33, 30, 28]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
