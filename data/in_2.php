9e17772a77f45dbad81af0d17384080153e60de0
Sallitrot <a01251527@itesm.mx>
2020-12-02 21:26:20 -0700

    Added in_2 file and updated in file

--- data/in_2.php
+++ data/in_2.php
@@ -5,17 +5,17 @@
     // tc1004b-reto.gearhostpreview.com/data/in.php?var1=95.23&var2=13.4
 
     // Verifying data
-    echo " [GET] var1: " . $_GET['var1'] . " [GET] var2: " . $_GET['var2'] . "<br/>";
+    echo " [GET] var3: " . $_GET['var3'] . " [GET] var4: " . $_GET['var4'] . "<br/>";
 
     // Parsing data
-    $var1 = $_GET['var1'];
-    $var2 = $_GET['var2'];
+    $var1 = $_GET['var3'];
+    $var2 = $_GET['var4'];
 
     // Only insert, if variables are not empty
-    if(isset($var1) || isset($var2)){
+    if(isset($var3) || isset($var4)){
         // MySQL injection query
-        $sql = "INSERT INTO sensor (sensor1Value, sensor2Value)
-        VALUES ('$var1','$var2')";
+        $sql = "INSERT INTO sensor (sensor3Value, sensor4Value)
+        VALUES ('$var3','$var4)";
         
         // Insert into database
         if (mysqli_query($conn, $sql)) {
@@ -27,5 +27,5 @@
         mysqli_close($conn);
         echo "[MySQL] Connection Closed <br/>";
     }
-
+    
 ?>
